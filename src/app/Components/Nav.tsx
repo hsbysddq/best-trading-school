5b1620173efe293fe55b1f5ead20de1db2ae5395
hsbysddq <88543652+hsbysddq@users.noreply.github.com>
2024-01-18 23:11:58 +0700

    edit nav

--- src/app/Components/Nav.tsx
+++ src/app/Components/Nav.tsx
@@ -34,7 +34,7 @@ const Nav = () => {
                       src="/burger-bar.png"
                       width={30}
                       height={30}
-                      alt="logo"
+                      alt="logoo"
                       className="focus:border-none active:border-none"
                     />
                   )}
@@ -44,7 +44,7 @@ const Nav = () => {
           </div>
           <div>
             <div
-              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              className={`flex-1 justify-self-center pb-2 mt-8 md:block md:pb-0 md:mt-0 ${
                 navbar ? "p-12 md:p-0 block" : "hidden"
               }`}
             >
